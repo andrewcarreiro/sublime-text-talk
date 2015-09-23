a77972bb85bc392facd5492881e887f6f65eaa8d
Andrew Carreiro <andrew@headfirst.ca>
2015-09-23 12:52:43 -0400

    more work on packages

--- 001_preamble.js
+++ 001_preamble.js
@@ -1,16 +1,18 @@
 /****************************************************************************
-*																			*
-*	This entire presentation exists in GitHub: 								*
-*	http://{TODO}															*
-*																			*
-*	Also, if you want to be able to switch your tabs like in Chrome,		*
-*	take the contents of the array and add them to your keymap.				*
-*	You can access your keymap by going to ⌘+SHIFT+P, and going to 			*
-*	"Preferences: Key Bindings - User"										*
-*																			*
+*                                                                           *
+*   This entire presentation exists in GitHub:                              *
+*   https://github.com/andrewcarreiro/sublime-text-talk                     *
+*   This presentation also expects Sublime Text 3, and will be giving       *
+*   OSX hotkeys.                                                            *
+*                                                                           *
+*   Also, if you want to be able to switch your tabs like in Chrome,        *
+*   take the contents of the array and add them to your keymap.             *
+*   You can access your keymap by going to ⌘+SHIFT+P, and going to          *
+*   "Preferences: Key Bindings - User"                                      *
+*                                                                           *
 *****************************************************************************/
 
 var inject_into_sublime_keymap = [
-	{ "keys": ["ctrl+tab"], "command": "next_view" },
-	{ "keys": ["ctrl+shift+tab"], "command": "prev_view" }
+    { "keys": ["ctrl+tab"], "command": "next_view" },
+    { "keys": ["ctrl+shift+tab"], "command": "prev_view" }
 ];
---
+++ 008_packages.md
@@ -0,0 +1,42 @@
+# PACKAGES
+
+## SidebarEnhancements
+- https://github.com/titoBouzout/SideBarEnhancements
+- Greatly expands the sidebar right click menu
+
+## Git Gutter
+- https://github.com/jisaacks/GitGutter
+- See your added, removed, and modified lines while you type in Sublime text
+
+## Origami
+- https://github.com/SublimeText/Origami
+- ⌘K to enter Origami mode, and then use the arrow keys to move between panes.
+	- *shift*: carry the current file to the destination
+	- *alt* (option): clone the current file to the destination
+	- *super*: create an adjacent pane
+	- *super+shift*: destroy an adjacent pane
+
+## AutoFileName
+- https://github.com/BoundInCode/AutoFileName
+- Typing <img src=" will allow you to pick from local files in html
+
+## Calculate
+- https://github.com/colinta/SublimeCalculate
+- 1+1
+- Highlight the above, and ⌘+SHIFT+P > Calculate. Very useful in conjunction with "count from one" and multi-select.
+
+## Emmet
+- https://github.com/sergeche/emmet-sublime
+- Huge system - http://emmet.io/
+
+## Syntax Highlighting
+- Sublime doesn't ship with everything, but you can search for Sass, Less, EJS, and pretty much any language you'd like highlighted
+
+### TODO:
+Sass/HTML/JS Beautifiers
+JS Hint Gutter
+
+Bracket Highlighter
+Syntax Highlighter for Sass
+Colorsublime - lets you preview and install new colour themes
+sublime-linter
